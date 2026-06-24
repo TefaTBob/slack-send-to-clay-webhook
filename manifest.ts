e3cbaaaa105dc5c32222176434a4e87f53405f4b
Ryan Ellis <rellis@ryanellis.local>
2026-06-24 09:26:41 -0400

    update app name and icon

--- manifest.ts
+++ manifest.ts
@@ -3,9 +3,9 @@ import { SendWebhookFunction } from "./functions/send_webhook.ts";
 import { SendWebhookWorkflow } from "./workflows/send_webhook_workflow.ts";
 
 export default Manifest({
-  name: "slack-send-to-webhook",
+  name: "Send to Clay Webhook",
   description: "Sends form data to a Clay webhook URL",
-  icon: "assets/default_new_app_icon.png",
+  icon: "assets/webhook.png",
   functions: [SendWebhookFunction],
   workflows: [SendWebhookWorkflow],
   outgoingDomains: ["api.clay.com"],
